df6fd1ab42bea4a2edf111370fc9bf636b1caefe
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2022-12-17 00:51:29 +0300

    finalized

---
+++ translate.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+# Replace YOUR_API_KEY with your actual API key
+api_key = '##'
+
+# Initialize an empty list to store the file names
+file_names = []
+
+# Walk through the directory tree and get the names of all files
+path = "files"
+for root, dirs, files in os.walk(path):
+    for file in files:
+        file_names.append(os.path.join(root, file))
+
+# Print the file names
+for filename in file_names:
+    # Load the HTML file
+    with open(filename, "r") as file:
+        html = file.read()
+
+    # Parse the HTML file
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Tags to find
+    tags = [
+        "h1",
+        "li",
+        "h3",
+        "label",
+        "option",
+        "th",
+        "button",
+        "small"
+    ]
+
+    # Set the source and target languages
+    src_lang = 'tr'
+    tgt_lang = 'en'
+
+    # Set the API endpoint and headers
+    endpoint = 'https://api.mymemory.translated.net/get'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    # Extract the text from the elements you want to translate
+    texts = [element.text for element in soup.find_all(tags)]
+
+    filtered_arr = list(filter(lambda x: '"' not in x, texts))
+    filtered_arr = list(filter(lambda x: '&' not in x, filtered_arr))
+    filtered_arr = list(filter(lambda x: '×' not in x, filtered_arr))
+    filtered_arr = list(filter(lambda x: "'" not in x, filtered_arr))
+    filtered_arr = list(filter(lambda x: "..." not in x, filtered_arr))
+    filtered_arr = list(filter(lambda x: "#" not in x, filtered_arr))
+    texts = filtered_arr
+
+    translated_texts = []
+    for text in texts:
+        text = text.strip()
+        if text == "":
+            print("It has only white spaces passing api call")
+            continue
+        print("text: " + text)
+
+        if text == "Delete":
+            print(filename)
+        # Set the request data
+        data = {
+            'q': text.lower(),
+            'langpair': f"{src_lang}|{tgt_lang}",
+            'key': api_key
+        }
+
+        # Make the request
+        # response = requests.post(endpoint, headers=headers, data=data)
+
+        # Extract the translated text from the response
+        # translation = response.json()['responseData']['translatedText'].capitalize()
+        translation = "passing go to api"
+
+        print("translation: " + translation)
+
+        # Replace the original text with the translated text
+        translated_texts.append(translation)
+        for element, translated_text in zip(soup.find_all(tags), translated_texts):
+            element.string = translated_text.capitalize()
+
+    # Extract the path and the file name
+    path, file_name = os.path.split(filename)
+
+    translated_path = "files-translated/" + path + "/"
+
+    if not os.path.exists(translated_path):
+        os.makedirs(translated_path)
+
+    # Save the modified HTML file
+    with open(translated_path + file_name, "x", encoding="utf-8") as file:
+        file.write(str(soup).replace("&gt;", ">").replace("&lt;", "<"))
